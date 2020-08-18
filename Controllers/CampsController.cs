229f4fcd87a9f1b8e8c5cae1295d12abe2116586
AnnaBarczyk <56827312+AnnaBarczyk@users.noreply.github.com>
2020-08-18 12:03:54 +0200

    Dodaj pliki projektów.

---
+++ Controllers/CampsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Moniker = "ATL2018", Name = "Atlanta CodeCamp" });
+        }
+    }
+}
